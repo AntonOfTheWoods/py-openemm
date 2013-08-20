67acdefdebecd79ec6ba4855230aa282bd01e271
Anton Melser <melser.anton@gmail.com>
2013-08-21 01:01:47 +0200

    Added set_subscriber_binding - useful for list-unsubscribe, among others

--- pyopenemm/webservice/service.py
+++ pyopenemm/webservice/service.py
@@ -22,7 +22,7 @@ class OpenEMM(object):
         self.username = WEBSERVICE_USER # openemm webservice username
         self.password = WEBSERVICE_PASSWORD # openemm webservice password
 
-    def find_subscriber(self,search):
+    def find_subscriber(self, search):
         """ Method to find openemm subscriber
         args : key column and value
         returns : subscriber/customer id
@@ -38,7 +38,7 @@ class OpenEMM(object):
 
         return customer_id
 
-    def get_subscriber(self,subscriber_id):
+    def get_subscriber(self, subscriber_id):
         """ Method to get subscriber with id
         args : id
         returns : dict of subscriber info
@@ -52,7 +52,7 @@ class OpenEMM(object):
 
         return customer_dict
 
-    def add_subscriber(self,user_dict,double_check,key_column,overwrite):
+    def add_subscriber(self, user_dict, double_check, key_column, overwrite):
         """ Method to add new subscriber
         args :
             user_dict = Dictionary containing user information
@@ -73,13 +73,35 @@ class OpenEMM(object):
 
         return customer_id
 
-    def delete_subscriber(self,subscriber_id):
+    def delete_subscriber(self, subscriber_id):
         """ Method to delete subscriber with id
         args : id
         returns : 1 for success and 0 for failure
         """
         try :
             customer_id = self.client.service.deleteSubscriber(self.username,self.password,subscriber_id)
+        except Exception, e:
+            # TODO : logger needs to be used
+            customer_id = 0
+
+        return customer_id
+
+    def set_subscriber_binding(self, subscriber_id, mailinglist_id, status, binding_type, remark, exit_mailing_id):
+        """ Method to update user bindings (mailing list opt-ins)
+        args :
+             subscriber_id - customer_id
+             mailinglist_id
+             status - 1: active, 2: bounced, 3: opt-out by admin, 4: opt-out by user
+             binding_type - 'A': admin, 'T': test, 'W': normal subscriber
+             remark - comment on the binding, like "opt-in by user", "opt-out by admin", etc.
+             exit_mailing_id - ID of the mailing which caused the opt-out or bounce (0 if unknown) for correct attribution in the stats.
+        returns : customer_id for success and 0 for failure
+        """
+        
+        media_type = 0 # Email, would be 1 for SMS but this is not available for the OSS version
+        
+        try :
+        customer_id = self.client.service.setSubscriberBinding(self.username,self.password,subscriber_id, mailinglist_id, media_type, status, binding_type, remark, exit_mailing_id)
         except Exception, e:
             # TODO : logger needs to be used
             customer_id = 0
